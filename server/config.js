0d4ccfece4b3e1ed5ca017da733ac041cca1f931
purismo <purismo@users.noreply.github.com>
2016-03-27 12:51:51 +0700

    edit ip

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://54.201.137.59:27017/mern-starter',
   port: process.env.PORT || 8000,
 };
 
